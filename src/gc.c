2284156f7cfd067ecc619dd35bac089900618909
CatEricka <32029431+CatEricka@users.noreply.github.com>
2021-03-22 12:26:25 +0800

    防止遍历 栈上变量保护表 时 NPE

--- src/gc.c
+++ src/gc.c
@@ -131,12 +131,10 @@ static object gc_mark(context_t context) {
 
     // env & dump stack ?
 
-    // 临时变量保护表
+    // C 调用栈上变量保护表
     for (gc_saves_list_t save = context->saves; save != NULL; save = save->next) {
-        if (save->illusory_object != NULL) {
-            if (is_object(*save->illusory_object)) {
-                gc_mark_one_start(context, *save->illusory_object);
-            }
+        if (save->illusory_object != NULL && is_object(*save->illusory_object)) {
+            gc_mark_one_start(context, *save->illusory_object);
         }
     }
     return IMM_TRUE;
@@ -235,9 +233,9 @@ static void gc_adjust_ref(context_t context) {
 
     // env & dump stack ?
 
-    // 临时变量保护表
+    // C 调用栈上变量保护表
     for (gc_saves_list_t save = context->saves; save != NULL; save = save->next) {
-        if (is_object(*save->illusory_object)) {
+        if (save->illusory_object != NULL && is_object(*save->illusory_object)) {
             *save->illusory_object = (*save->illusory_object)->forwarding;
         }
     }
